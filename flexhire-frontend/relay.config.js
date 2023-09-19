44bf3ffcda5f0cdc9d58baf5b6fa19001e5398e7
Rafael Polonio <rafael.polonio@gmail.com>
2023-09-19 11:41:18 -0300

    configuring relay

---
+++ flexhire-frontend/relay.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  src: "./src",
+  language: "javascript", // "javascript" | "typescript" | "flow"
+  schema: "./src/graphql/schema.graphql",
+  excludes: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+}
